98af39715f1a36648b94df5de406c3d7403ba99a
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2020-11-19 11:37:39 +0100

    modified say hello to greet arbitrary people, added range function, also added tests

--- tests/test_your_python_project.py
+++ tests/test_your_python_project.py
@@ -3,6 +3,8 @@
 """Tests for `your_python_project` package."""
 
 import pytest
+import numpy as np
+import numpy.testing as npt
 
 
 from your_python_project import your_python_project
@@ -20,3 +22,15 @@ def test_hello_fabian():
     string = your_python_project.say_hello('Fabian')
 
     assert expected == string
+
+
+def test_error():
+    with pytest.raises(ValueError):
+        your_python_project.say_hello(1)
+
+
+def test_a_range():
+    expected = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    actual = your_python_project.giva_me_a_range()
+
+    npt.assert_allclose(actual, expected, rtol=1e-9)
--- your_python_project/your_python_project.py
+++ your_python_project/your_python_project.py
@@ -1,6 +1,27 @@
 """Main module."""
 
+import numpy as np
+
 
 def say_hello(name='World'):
+    """Say hello to someone
+
+    Parameters
+    ----------
+    name : string
+        A string containing the name of the person who is to greeted.
+
+    Returns
+    -------
+    string : string
+        The greetings string.
+
+    """
+    if not isinstance(name, str):
+        raise ValueError("I need a string.")
     string = 'Hello {}!'.format(name)
     return string
+
+
+def giva_me_a_range():
+    return np.arange(1, 10) + 1e-10
